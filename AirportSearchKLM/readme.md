f34f1f19e48d92b03039f415c0cd47440e5df4fe
vamshi krishna <Lakshmiser.thummala9@gmail.com>
2019-05-16 19:10:54 +0530

    delete

--- AirportSearchKLM/readme.md
+++
@@ -1,2 +0,0 @@
-# AirportSearchKLM
-AirportSearchKLM
--- AirportSearchKLM/src/main/java/com/KLM/auth/ApiAuth.java
+++
@@ -1,8 +0,0 @@
-package com.KLM.auth;
-
-import org.json.JSONException;
-import com.mashape.unirest.http.exceptions.UnirestException;
-
-public interface ApiAuth {
-	String getToken() throws UnirestException, JSONException;
-}
